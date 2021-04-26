e50beddbd73e89796215360e3c198ece042b7fea
VKaptur <vkaptur@gomel.iba.by>
2021-04-26 11:42:17 +0300

    task1

---
+++ src/FHcomponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+export default function FHcomponent() {
+    return(
+
+<div className="header">
+<div className="text1">
+  Your advertising could be here
+</div>
+</div>
+  )
+} 
---
+++ src/Fcomponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+export default function Fcomponent() {
+    return(
+
+<div className="card">
+  <h1>Caption</h1>
+  <hr></hr>
+  <p className="title">Some text</p>
+  <p><button>Button</button></p>
+</div>
+  )
+} 
+
+
