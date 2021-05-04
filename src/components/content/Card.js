2032222ba1787d3503cd78caa176e9b3553dbac9
VKaptur <vkaptur@gomel.iba.by>
2021-05-04 14:10:12 +0300

    task2

--- src/components/content/Card.js
+++ src/components/content/Card.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const Card = props => {
+  const [style, setStyle] = useState(false);
+  const clickHandler = () => {
+    setStyle(!style);
+  };
+
   return (
-    <div className="card">
+    <div className={`card${style.toString()}`}>
+      <input
+        type="checkbox"
+        className="checkbox"
+        id="checkbox"
+        onClick={clickHandler}
+      />
+      <label htmlFor="checkbox" />
       <div className="caption">{props.card.caption}</div>
       <hr />
       <p className="title">{props.card.text}</p>
